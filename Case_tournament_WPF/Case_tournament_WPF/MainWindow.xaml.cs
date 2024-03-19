1af00895f0a48b88d17d99a566f444fbe0ba2ad6
nikita010703 <90619602+nikita010703@users.noreply.github.com>
2024-03-19 18:14:59 +0500

    Update to read data from xlsx

--- Case_tournament_WPF/Case_tournament_WPF/MainWindow.xaml.cs
+++ Case_tournament_WPF/Case_tournament_WPF/MainWindow.xaml.cs
@@ -1,23 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Office.Interop.Excel;
 
 namespace Case_tournament_WPF {
 
-    public partial class MainWindow : Window, INotifyPropertyChanged  {
+    public partial class MainWindow : System.Windows.Window, INotifyPropertyChanged  {
 
         private ModelNPV model;
         public ModelNPV Model {
@@ -52,9 +43,9 @@ namespace Case_tournament_WPF {
         private double npv;
         private const int startYear = 2020;
         private const int totalYears = 30;
-        private readonly List<int> incomes;
-        private readonly List<int> outcomes;
-        private readonly List<int> netCashFlow;
+        private readonly List<double> incomes;
+        private readonly List<double> outcomes;
+        private readonly List<double> netCashFlow;
 
         public int Year {
             get { return year; }
@@ -67,7 +58,7 @@ namespace Case_tournament_WPF {
         public float DiscountStake {
             get { return discountStake; }
             set {
-                discountStake = value;
+                discountStake = !(value < 0) ? value : 0;
                 OnPropertyChanged("DiscountStake");
             }
         }
@@ -81,22 +72,46 @@ namespace Case_tournament_WPF {
         }
 
         public ModelNPV() {
-            incomes = Enumerable.Repeat(1000, totalYears).ToList();
-            outcomes = Enumerable.Repeat(0, totalYears).ToList();
-            outcomes[2] = outcomes[3] = 500;
-            netCashFlow = new List<int>();
-            for (int i = 0; i < totalYears; ++i) {
-                netCashFlow.Add(incomes[i] - outcomes[i]);
-            }
+            incomes = new List<double>();
+            outcomes = new List<double>();
+            netCashFlow = new List<double>();
         }
         
         public void CalculateNPV() {
-            double npv = netCashFlow[0] * (1 / (1 + discountStake));
+            string file = Directory.GetCurrentDirectory() + "\\Source.xlsx";
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Workbook wb = excel.Workbooks.Open(file);
+            Worksheet ws = wb.Worksheets[2];
 
+            Range income = ws.Range["B2:B32"];
+            foreach (double cell in income.Value) {
+                incomes.Add((int)cell);
+            }
+
+            Range outcome = ws.Range["C2:C32"];
+            foreach (double cell in outcome.Value) {
+                //int tmp;
+                outcomes.Add((int)cell);
+            }
+
+            for (int i = 0; i < totalYears; ++i) {
+                netCashFlow.Add(incomes[i] - outcomes[i]);
+            }
+
+            double npv = netCashFlow[0] * (1 / (1 + discountStake));
             for (int i = 1; i < year - startYear - 1; ++i) {
                 npv += netCashFlow[i] * (1 / Math.Pow(1 + discountStake, i + 1));
             }
             NPV = npv;
+
+            wb.Close();
+            excel.Quit();
+
+            Marshal.ReleaseComObject(outcome);
+            Marshal.ReleaseComObject(income);
+            Marshal.ReleaseComObject(ws);
+            Marshal.ReleaseComObject(wb);
+            Marshal.ReleaseComObject(excel);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
